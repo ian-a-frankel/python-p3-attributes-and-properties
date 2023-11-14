0e705cac02e37201b9bb74407f1b8ce55bad8ad3
Ian Frankel <ian.frankel@gmail.com>
2023-11-14 15:02:43 -0500

    everything approved

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,18 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name='Cerberus', breed='Pug'):
+        if type(name) != str or name == '':
+            print('Name must be string between 1 and 25 characters.')
+        elif len(name) > 25:
+            print('Name must be string between 1 and 25 characters.')
+        else:
+            self.name = name
+
+        if breed not in APPROVED_BREEDS:
+            print('Breed must be in list of approved breeds.')
+        else:
+            self.breed = breed
+
+
+        
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,16 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = "Ian Alexander The Great", job = 'self.name = name'):
+        if type(name) != str or name == '':
+            print('Name must be string between 1 and 25 characters.')
+        elif len(name) < 26:
+            self.name = name.title()
+        else:
+            print('Name must be string between 1 and 25 characters.')
+
+        if job not in APPROVED_JOBS:
+            print('Job must be in list of approved jobs.')
+        else:
+            self.job = job
+
